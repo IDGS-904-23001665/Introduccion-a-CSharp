003daac6ba6a2a63978b15021c5df40bb320746e
Daniel <Daniel@DESKTOP-BUJUE0B>
2026-04-30 19:17:21 -0600

    Agregar archivos de proyecto.

---
+++ IntroduccionCSharp/Controllers/PruebaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionCSharp.Controllers
+{
+    public class PruebaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Suma(String x, String y, String r1)
+        {
+            int res = Convert.ToInt16(x) + Convert.ToInt16(y);
+            ViewBag.Res = Convert.ToString(res);
+
+            return View();
+        }
+    }
+}
