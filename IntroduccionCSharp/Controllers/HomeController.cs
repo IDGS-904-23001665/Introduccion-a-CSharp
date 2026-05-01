6fd4ad5d82b10cba03921c551d93f508ebb77c99
Daniel <Daniel@DESKTOP-BUJUE0B>
2026-04-30 19:17:21 -0600

    Introduccion a C# 30/04/2026

---
+++ IntroduccionCSharp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionCSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "juan daniel nachez";
+            ViewBag.grupo = "IDGS904";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ IntroduccionCSharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntroduccionCSharp.MvcApplication" Language="C#" %>
