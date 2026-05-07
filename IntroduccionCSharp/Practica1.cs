a36854d130e646595ddc5b0afbbff9910a6d2d50
IDGS-904-23001665 <Daniel@DESKTOP-BUJUE0B>
2026-05-06 21:00:59 -0600

    Modelos 06/05/2026

---
+++ IntroduccionCSharp/Practica1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionCSharp.Models
+{
+    internal class Practica1
+    {
+        static void Main(String[] args)
+        {
+            String dato1;
+            int cal1 = 0;
+            int cal2, cal3, resultado;
+
+            Console.WriteLine("Ingresa la primera calificacion: ");
+            dato1 = Console.ReadLine();
+            cal1 = Convert.ToInt32(dato1);
+            Console.WriteLine("Ingresa la segunda calificacion: ");
+            dato1 = Console.ReadLine();
+            cal2 = Convert.ToInt32(dato1);
+            int suma = cal1 + cal2;
+
+            Console.WriteLine("La suma de las calificaciones es: " + suma);
+
+            // If
+            if (cal1 > cal2)
+            {
+                Console.WriteLine("La nota mayor es: " + cal1);
+            }
+            else
+            {
+                Console.WriteLine("La nota mayor es: " + cal2);
+            }
+
+            // While
+            int x = 1;
+            while (x <= 100)
+            {
+                Console.Write(x);
+                Console.Write(" - ");
+                x++;
+            }
+            Console.Write("\n");
+            // Do while
+            int y = 1;
+            do
+            {
+                Console.Write(y);
+                Console.Write(" - ");
+                y++;
+            } while (y <= 100);
+
+            Console.Write("\n");
+            //For
+            int f;
+            for (f = 1; f <= 100; f++)
+            {
+                Console.Write(f);
+                Console.Write(" - ");
+            }
+        }
+    }
+}
---
+++ IntroduccionCSharp/Practica2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionCSharp.Models
+{
+    class Practica2
+    {
+        String nombre;
+        private int edad;
+        protected String carrera;
+
+        public void ingresarDatos()
+        {
+            Console.WriteLine("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa tu carrera: ");
+            carrera = Console.ReadLine();
+        }
+
+        public void mostrarDatos()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Carrera: " + carrera);
+        }
+
+        public void Main(String[] args)
+        {
+            Practica2 p2 = new Practica2();
+            p2.ingresarDatos();
+            p2.mostrarDatos();
+        }
+    }
+}
